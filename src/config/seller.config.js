eb7d1d658656e78481266651fe98e46e649cb67a
bharathiuyarchi <bharathi@uyarchi.com>
2023-07-06 12:03:56 +0530

    sdfsd

--- src/config/seller.config.js
+++ src/config/seller.config.js
@@ -3,23 +3,23 @@ var urlencode = require('urlencode');
 const urlencodeed = require('rawurlencode');
 const moment = require('moment');
 const Otp = async (mobile, user) => {
-  var sender = 'txtlcl';
+  // var sender = 'txtlcl';
   const contact = mobile;
-  numbers = '91' + contact;
-  apiKey = urlencode('NTgzOTZiMzY3MTQ4MzI0ODU1NmI0NDZhNDQ3NTQ5NmY=');
-  sender = urlencode('UYARBZ');
+  // numbers = '91' + contact;
+  // apiKey = urlencode('NTgzOTZiMzY3MTQ4MzI0ODU1NmI0NDZhNDQ3NTQ5NmY=');
+  // sender = urlencode('UYARBZ');
   let OTPCODE = Math.floor(100000 + Math.random() * 900000);
-  message = urlencodeed(
-    'Dear ' +
-    user.contactName +
-    ', thank you for registering with Kapture(An Uyarchi Solutions company). Your OTP for logging into the account is ' +
-    OTPCODE +
-    ' .'
-  );
-  data = 'send/?apikey=' + apiKey + '&numbers=' + numbers + '&sender=' + sender + '&message=' + message;
-  var options = 'https://api.textlocal.in/' + data;
+  // message = urlencodeed(
+  //   'Dear ' +
+  //   user.contactName +
+  //   ', thank you for registering with Kapture(An Uyarchi Solutions company). Your OTP for logging into the account is ' +
+  //   OTPCODE +
+  //   ' .'
+  // );
+  // data = 'send/?apikey=' + apiKey + '&numbers=' + numbers + '&sender=' + sender + '&message=' + message;
+  // var options = 'https://api.textlocal.in/' + data;
   await saveOtp(contact, OTPCODE, user);
-  https.request(options, callback).end();
+  // https.request(options, callback).end();
   return 'OTP Send Successfully';
 };
 
