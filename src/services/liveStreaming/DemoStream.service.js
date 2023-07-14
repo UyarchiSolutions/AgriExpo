45568903e7a4f203a8f56484106e27cf8bea10de
bharathiuyarchi <bharathi@uyarchi.com>
2023-07-14 12:43:04 +0530

    D

--- src/services/liveStreaming/DemoStream.service.js
+++ src/services/liveStreaming/DemoStream.service.js
@@ -297,7 +297,7 @@ const send_otp = async (stream) => {
 
   let message = `Dear ${token.name},thank you for the registration to the event AgriExpoLive2023 .Your OTP for logging into the account is ${OTPCODE}- AgriExpoLive2023(An Ookam company event)`;
   let reva = await axios.get(
-    `http://panel.smsmessenger.in/api/mt/SendSMS?user=ookam&password=ookam&senderid=OOKAMM&channel=Trans&DCS=0&flashsms=0&number=${token.phoneNumber}&text=${message}&route=6&peid=1707168908130209371&DLTTemplateId=1701168700339760716`,
+    `http://panel.smsmessenger.in/api/mt/SendSMS?user=ookam&password=ookam&senderid=OOKAMM&channel=Trans&DCS=0&flashsms=0&number=${token.phoneNumber}&text=${message}&route=6&peid=1701168700339760716&DLTTemplateId=1707168908130209371`,
   )
   // return reva.data;
 
