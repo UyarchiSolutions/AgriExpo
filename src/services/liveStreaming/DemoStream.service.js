8e9708084e09f85df4ff433f91430472b70acb89
bharathiuyarchi <bharathi@uyarchi.com>
2023-07-31 17:57:46 +0530

    sd

--- src/services/liveStreaming/DemoStream.service.js
+++ src/services/liveStreaming/DemoStream.service.js
@@ -2155,7 +2155,7 @@ const recording_query = async (id, agoraToken) => {
     { headers: { Authorization } }
   );
   console.log(query.data)
-  console.log(query.data.fileList:)
+  console.log(query.data.fileList)
   // token.videoLink = query.data.serverResponse.fileList;
   token.recoredStart = 'query';
   token.save();
