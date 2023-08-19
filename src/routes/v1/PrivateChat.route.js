fbdc9d54e1c1a47eb882a4d9d8236bdfed335b33
bharathiuyarchi <bharathi@uyarchi.com>
2023-08-19 15:47:49 +0530

    as

--- src/routes/v1/PrivateChat.route.js
+++ src/routes/v1/PrivateChat.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const shopverify = require('../../controllers/shoptokenverify.controller');
 
 const PrivateChat = require('../../controllers/PrivateChat.controller');
 
 
-router.route('/intraction/exp').post(PrivateChat.intraction_exhibitor);
+router.route('/intraction/exp').post(shopverify, PrivateChat.intraction_exhibitor);
 
 module.exports = router;
