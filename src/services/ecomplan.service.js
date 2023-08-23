5d75d13a20490418d21f9bc217c14a0e9ad0f930
bharathiuyarchi <bharathi@uyarchi.com>
2023-08-23 17:23:37 +0530

    As

--- src/services/ecomplan.service.js
+++ src/services/ecomplan.service.js
@@ -3396,6 +3396,26 @@ const get_subhost_token = async (req, userId) => {
       },
     },
     {
+      $lookup: {
+        from: 'temptokens',
+        localField: '_id',
+        foreignField: 'streamId',
+        pipeline: [{ $match: { $and: [{ type: { $eq: 'raiseHands' } }] } }],
+        as: 'raiseID',
+      },
+    },
+    {
+      $unwind: {
+        preserveNullAndEmptyArrays: true,
+        path: '$raiseID',
+      },
+    },
+    {
+      $addFields: {
+        raiseUID: { $ifNull: ['$raiseID.Uid', 0] },
+      },
+    },
+    {
       $project: {
         _id: 1,
         supplierName: '$suppliers.contactName',
