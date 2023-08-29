bec5be7bf93c82254d5fd282df163cca1988ff90
bharathiuyarchi <bharathi@uyarchi.com>
2023-08-29 22:46:06 +0530

    SD

--- src/services/ccavenue.service.js
+++ src/services/ccavenue.service.js
@@ -7,46 +7,74 @@ var crypto = require('crypto');
 
 const axios = require('axios');
 const get_paymnent_url = async () => {
+    // const merchantId = '2742878';
+    // const accessCode = 'AVRI05KH14CC73IRCC';
+    // const amount = 100;
+    // const orderId = uuid.v4();
+    // const workingID = "6EB13DAEA5810ACB66C7C95BDD4D2684";
+    // const paymentData = {
+    //     amount: 100, // Set the payment amount
+    //     orderId: orderId, // Set a unique order ID
+    //     currency: 'INR',
+    //     redirectUrl: 'https://exhibitor.agriexpo.live/', // URL to redirect after payment
+    //     cancelUrl: 'https://exhibitor.agriexpo.live/', // URL to redirect if payment is cancelled
+    // };
+
+    // // Create a secure hash
+    // const secureHash = crypto.createHash('sha256')
+    //     .update(`${merchantId}|${paymentData.orderId}|${paymentData.amount}|${paymentData.redirectUrl}|${workingID}`)
+    //     .digest('hex');
+
+    // // API request payload
+    // const requestData = {
+    //     merchant_id: merchantId,
+    //     order_id: paymentData.orderId,
+    //     amount: paymentData.amount,
+    //     currency: paymentData.currency,
+    //     redirect_url: paymentData.redirectUrl,
+    //     cancel_url: paymentData.cancelUrl,
+    //     secure_hash: secureHash,
+    // };
+    // try {
+
+    //     console.log(requestData)
+    //     const response = await axios.post('https://secure.ccavenue.com/transaction/initiate.do', requestData);
+    //     const paymentPageUrl = response.data;
+    //     console.log(paymentPageUrl)
+    //     console.log(response.data)
+    //     return response.data;
+
+    // } catch (error) {
+    //     console.log(error)
+    //     return error;
+    // }
+    // Gather order information
+    const orderAmount = 1000; // Example order amount in paise
     const merchantId = '2742878';
     const accessCode = 'AVRI05KH14CC73IRCC';
-    const amount = 100;
+    const workingKey = '6EB13DAEA5810ACB66C7C95BDD4D2684';
     const orderId = uuid.v4();
-    const workingID = "6EB13DAEA5810ACB66C7C95BDD4D2684";
-    const paymentData = {
-        amount: 100, // Set the payment amount
-        orderId: orderId, // Set a unique order ID
-        currency: 'INR',
-        redirectUrl: 'https://exhibitor.agriexpo.live/', // URL to redirect after payment
-        cancelUrl: 'https://exhibitor.agriexpo.live/', // URL to redirect if payment is cancelled
-    };
 
-    // Create a secure hash
-    const secureHash = crypto.createHash('sha256')
-        .update(`${merchantId}|${paymentData.orderId}|${paymentData.amount}|${paymentData.redirectUrl}|${workingID}`)
-        .digest('hex');
+    // Create a secure hash to send along with the request
+    const hashString = `${merchantId}|${orderAmount}||${orderId}|bharathiraja996574@gmail.com|919965740303|||||||||||${workingKey}`;
+    const secureHash = crypto.createHash('sha256').update(hashString).digest('hex');
 
-    // API request payload
-    const requestData = {
-        merchant_id: merchantId,
-        order_id: paymentData.orderId,
-        amount: paymentData.amount,
-        currency: paymentData.currency,
-        redirect_url: paymentData.redirectUrl,
-        cancel_url: paymentData.cancelUrl,
-        secure_hash: secureHash,
-    };
+    // Make the API request
     try {
+        const response = await axios.post('https://secure.ccavenue.com/transaction/transaction.do', {
+            merchant_id: merchantId,
+            order_id: orderId,
+            amount: orderAmount,
+            currency: 'INR',
+            redirect_url: 'https://exhibitor.agriexpo.live',
+            cancel_url: 'https://exhibitor.agriexpo.live',
+            secure_hash: secureHash,
+        });
 
-        console.log(requestData)
-        const response = await axios.post('https://secure.ccavenue.com/transaction/initiate.do', requestData);
-        const paymentPageUrl = response.data;
-        console.log(paymentPageUrl)
-        console.log(response.data)
-        return response.data;
-
+        // Process the response and handle the payment result
+        console.log(response.data);
     } catch (error) {
-        console.log(error)
-        return error;
+        console.error(error);
     }
 };
 
