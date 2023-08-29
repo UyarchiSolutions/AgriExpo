cf3e462ff6a97449e9393ed8636e6f2904669f1c
bharathiuyarchi <bharathi@uyarchi.com>
2023-08-29 14:16:27 +0530

    sd

--- src/services/ecomplan.service.js
+++ src/services/ecomplan.service.js
@@ -3460,6 +3460,11 @@ const get_subhost_token = async (req, userId) => {
       },
     },
     {
+      $addFields: {
+        allot_host_1_details: { $cond: { if: { $eq: ['$allot_host_1', 'my self'] }, then: '$suppierId', else: '$allot_host_1'} },
+      },
+    },
+    {
       $project: {
         _id: 1,
         supplierName: '$suppliers.contactName',
@@ -3494,7 +3499,7 @@ const get_subhost_token = async (req, userId) => {
         raiseUID: 1,
         RaiseHands: '$purchasedplans.RaiseHands',
         current_raise: 1,
-        allot_host_1:1
+        allot_host_1: 1
       },
     },
   ]);
