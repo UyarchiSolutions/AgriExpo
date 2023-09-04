7dcf375494115c832f43e5c8f0bc55bc080dbb62
bharathiuyarchi <bharathi@uyarchi.com>
2023-09-04 17:18:37 +0530

    Sd

--- src/services/ccavenue.service.js
+++ src/services/ccavenue.service.js
@@ -13,63 +13,64 @@ const get_paymnent_url = async (aa, dd, res) => {
     const accessCode = 'AVRI05KH14CC73IRCC';
     const workingKey = '6EB13DAEA5810ACB66C7C95BDD4D2684';
     const orderId = uuid.v4();
-    const baseUrl = 'https://test.ccavenue.com/transaction/transaction.do';
+    const baseUrl = 'https://secure.ccavenue.com/transaction/transaction.do';
 
-    // const paymentData = {
-    //     merchant_id: merchantId,
-    //     order_id: orderId,
-    //     amount: '100.00',
-    //     currency: 'INR',
-    //     redirect_url: 'https://exhibitor.agriexpo.live/',
-    //     cancel_url: 'https://exhibitor.agriexpo.live/',
-    //     language: 'EN',
-    //     billing_name: 'John Doe',
-    //     billing_address: '123 Main St',
-    //     billing_city: 'chennai',
-    //     billing_state: 'tamilnadu',
-    //     billing_zip: '600017',
-    //     billing_country: 'India',
-    //     billing_tel: '9965740303',
-    //     billing_email: 'bharathiraja996574@gmail.com',
-    // };
     const paymentData = {
         merchant_id: merchantId,
         order_id: orderId,
         amount: '100.00',
         currency: 'INR',
-        redirect_url: 'https://exhibitor.agriexpo.live/',
-        cancel_url: 'https://exhibitor.agriexpo.live/',
-        // Add more payment data as needed
+        redirect_url: 'https://agriexpo.click/',
+        cancel_url: 'https://agriexpo.live/',
+        language: 'EN',
+        billing_name: 'John Doe',
+        billing_address: '123 Main St',
+        billing_city: 'chennai',
+        billing_state: 'tamilnadu',
+        billing_zip: '600017',
+        billing_country: 'India',
+        billing_tel: '9965740303',
+        billing_email: 'bharathiraja996574@gmail.com',
     };
-
-    // Create a secure hash to send with the request
-    const secureHash = createSecureHash(paymentData, workingKey);
-    paymentData.secure_hash = secureHash;
-    let data = await axios.post('https://test.ccavenue.com/transaction/transaction.do', paymentData)
-        .then((response) => {
-            // Handle the CCAvenue response
-            console.log(response.data);
+    const crypto = require('crypto');
+    const keys = Object.keys(paymentData).sort();
+    const values = keys.map(key => paymentData[key]);
+    const concatenatedString = values.join('|');
+    const checksum = crypto
+        .createHmac('sha256', workingKey)
+        .update(concatenatedString)
+        .digest('hex')
+        .toUpperCase();
+    paymentData.checksum = checksum;
+    let data = await axios.post(baseUrl, paymentData)
+        .then(response => {
+            console.log('Payment URL:', response.data);
             return response.data;
         })
-        .catch((error) => {
-            // Handle errors
-            console.error(error);
-            return error
+        .catch(error => {
+            // console.error('Payment Error:', error);
+            return error;
         });
+
     return data;
+
+
+    // axios.post('https://secure.ccavenue.com/transaction/transaction.do', paymentData)
+    //     .then(response => {
+    //         // Handle the response from CCAvenue
+    //         console.log(response.data);
+    //     })
+    //     .catch(error => {
+    //         // Handle errors
+    //         console.error(error);
+    //     });
+
+    return paymentData;
+
 };
 
 
 
-function createSecureHash(data, workingKey) {
-    // Concatenate the data fields as per CCAvenue's requirements
-    const concatenatedData = `${data.merchant_id}|${data.order_id}|${data.amount}|${data.redirect_url}|${workingKey}`;
-
-    // Create an MD5 hash of the concatenated data
-    const hash = crypto.createHash('md5').update(concatenatedData).digest('hex');
-
-    return hash;
-}
 
 module.exports = {
     get_paymnent_url
