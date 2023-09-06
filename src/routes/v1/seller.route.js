0cc278c3b1410de56c0964b1e03605b2ec6dceb2
bharathiuyarchi <bharathi@uyarchi.com>
2023-09-06 17:58:46 +0530

    sd

--- src/routes/v1/seller.route.js
+++ src/routes/v1/seller.route.js
@@ -16,6 +16,7 @@ router.route('/create/subhost').post(SellerAuth, SellerController.createSubhost)
 router.route('/getall/subhost').get(SellerAuth, SellerController.getsubhostAll);
 router.route('/get/subhost/free').get(SellerAuth, SellerController.subhost_free_users);
 router.route('/disabled/subhost').put(SellerAuth, SellerController.disabled_hosts);
+router.route('/delete/subhost').delete(SellerAuth, SellerController.delete_hosts);
 router.route('/get/single/host').get(SellerAuth, SellerController.get_single_host);
 router.route('/update/single/host').put(SellerAuth, SellerController.update_single_host);
 
