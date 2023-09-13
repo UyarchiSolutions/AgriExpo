67fd82658ad462ae6c99ed1bee8d9676a04ca9fb
bharathidev0303 <bharathiraja996574@gmail.com>
2023-09-13 09:27:05 +0530

    dS

--- src/ccavRequestHandler.js
+++ src/ccavRequestHandler.js
@@ -5,22 +5,25 @@ var http = require('http'),
 
 exports.postReq = function (request, response) {
     var body = '',
-	workingKey = '',		//Put in the 32-Bit key shared by CCAvenues.
-	accessCode = '',		//Put in the access code shared by CCAvenues.
-	encRequest = '',
-	formbody = '';
-				
+        workingKey = 'B0050D8C882D10898AE305B141D27C8C',	//Put in the 32-Bit key shared by CCAvenues.
+        accessCode = 'AVOI05KI17AK41IOKA',			//Put in the Access Code shared by CCAvenues.
+        encRequest = '',
+        formbody = '<h1>hello<h1>';
+    // console.log(workingKey)
+    // console.log(accessCode)
+    // console.log(request)
+
     request.on('data', function (data) {
-	body += data;
-	encRequest = ccav.encrypt(body,workingKey); 
-	POST =  qs.parse(body);
-	formbody = '<html><head><title>Sub-merchant checkout page</title><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script></head><body><center><!-- width required mininmum 482px --><iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id='+POST.merchant_id+'&encRequest='+encRequest+'&access_code='+accessCode+'"></iframe></center><script type="text/javascript">$(document).ready(function(){$("iframe#paymentFrame").load(function() {window.addEventListener("message", function(e) {$("#paymentFrame").css("height",e.data["newHeight"]+"px"); }, false);}); });</script></body></html>';
+        body += data;
+        encRequest = ccav.encrypt(body, workingKey);
+        POST = qs.parse(body);
+        formbody = '<html><head><title>Sub-merchant checkout page</title><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script></head><body><center><!-- width required mininmum 482px --><iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id=' + POST.merchant_id + '&encRequest=' + encRequest + '&access_code=' + accessCode + '"></iframe></center><script type="text/javascript">$(document).ready(function(){$("iframe#paymentFrame").load(function() {window.addEventListener("message", function(e) {$("#paymentFrame").css("height",e.data["newHeight"]+"px"); }, false);}); });</script></body></html>';
     });
 
     request.on('end', function () {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-	response.write(formbody);
-	response.end();
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write(formbody);
+        response.end();
     });
-   return; 
+    return;
 };
