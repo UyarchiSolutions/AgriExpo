81d17802533143ff6e8af1205db7d7a16713de72
bharathidev0303 <bharathiraja996574@gmail.com>
2023-09-13 09:38:07 +0530

    dS

--- src/ccavRequestHandler.js
+++ src/ccavRequestHandler.js
@@ -18,9 +18,15 @@ exports.postReq = function (request, response) {
         encRequest = ccav.encrypt(body, workingKey);
         POST = qs.parse(body);
         console.log(POST)
-        formbody = '<html><head><title>Sub-merchant checkout page</title><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script></head><body><center><!-- width required mininmum 482px --><iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id=' + POST.merchant_id + '&encRequest=' + encRequest + '&access_code=' + accessCode + '"></iframe></center><script type="text/javascript">$(document).ready(function(){$("iframe#paymentFrame").load(function() {window.addEventListener("message", function(e) {$("#paymentFrame").css("height",e.data["newHeight"]+"px"); }, false);}); });</script></body></html>';
+        formbody = '<html><head><title>Sub-merchant checkout page</title></head><body><center><!-- width required mininmum 482px --><iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id=' + POST.merchant_id + '&encRequest=' + encRequest + '&access_code=' + accessCode + '"></iframe></center><script type="text/javascript">document.addEventListener("DOMContentLoaded", function () { var paymentFrame = document.querySelector("iframe#paymentFrame");paymentFrame.addEventListener("load", function () {window.addEventListener("message", function (e) {if (e.data.newHeight) { paymentFrame.style.height = e.data.newHeight + "px";}}, false);});});</script></body></html>';
     });
-
+    
+      
+      
+      
+      
+      
+      
     request.on('end', function () {
         response.writeHeader(200, { "Content-Type": "text/html" });
         response.write(formbody);
