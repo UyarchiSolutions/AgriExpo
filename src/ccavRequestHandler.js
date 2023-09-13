2c1ec7a1112b866f475b9083a8656d114f373f8c
bharathidev0303 <bharathiraja996574@gmail.com>
2023-09-13 09:25:38 +0530

    dS

--- src/ccavRequestHandler.js
+++ src/ccavRequestHandler.js
@@ -5,30 +5,22 @@ var http = require('http'),
 
 exports.postReq = function (request, response) {
     var body = '',
-        workingKey = 'B0050D8C882D10898AE305B141D27C8C',	//Put in the 32-Bit key shared by CCAvenues.
-        accessCode = 'AVOI05KI17AK41IOKA',			//Put in the Access Code shared by CCAvenues.
-        encRequest = '',
-        formbody = '<h1>hello<h1>';
-    // console.log(workingKey)
-    // console.log(accessCode)
-    // console.log(request)
-
+	workingKey = '',		//Put in the 32-Bit key shared by CCAvenues.
+	accessCode = '',		//Put in the access code shared by CCAvenues.
+	encRequest = '',
+	formbody = '';
+				
     request.on('data', function (data) {
-        // console.log(data)
-        bufferToJson = data.toString();
-        bufferToJson = JSON.stringify(bufferToJson)
-        bufferToJson = JSON.parse(bufferToJson)
-        console.log(bufferToJson)
-        body += data;
-        encRequest = ccav.encrypt(body, workingKey);
-        // console.log(encRequest)
-        formbody = '<form id="nonseamless" method="post" name="redirect" action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
+	body += data;
+	encRequest = ccav.encrypt(body,workingKey); 
+	POST =  qs.parse(body);
+	formbody = '<html><head><title>Sub-merchant checkout page</title><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script></head><body><center><!-- width required mininmum 482px --><iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id='+POST.merchant_id+'&encRequest='+encRequest+'&access_code='+accessCode+'"></iframe></center><script type="text/javascript">$(document).ready(function(){$("iframe#paymentFrame").load(function() {window.addEventListener("message", function(e) {$("#paymentFrame").css("height",e.data["newHeight"]+"px"); }, false);}); });</script></body></html>';
     });
 
     request.on('end', function () {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(formbody);
-        response.end();
+        response.writeHeader(200, {"Content-Type": "text/html"});
+	response.write(formbody);
+	response.end();
     });
-    return;
+   return; 
 };
