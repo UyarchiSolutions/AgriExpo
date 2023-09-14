8c9366fe82ad52e4a193f8400e1704c3c466f543
bharathiuyarchi <bharathi@uyarchi.com>
2023-09-14 23:09:36 +0530

    sd

--- src/ccavRequestHandler.js
+++ src/ccavRequestHandler.js
@@ -60,8 +60,8 @@ exports.success_recive = function (request, response) {
         pData = pData.replace(/&/gi, '</td></tr><tr><td>')
         pData = pData + '</td></tr></table>'
         htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>' + pData + '</center><br></body></html>';
-        // response.writeHeader(200, { "Content-Type": "text/html" });
-        response.send(ccavResponse);
-        // response.end();
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.send(htmlcode);
+        response.end();
     });
 };
