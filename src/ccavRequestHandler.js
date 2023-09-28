2e527860c2abf8599d9806e6918c0cf009e3eff6
bharathiuyarchi <bharathi@uyarchi.com>
2023-09-28 17:35:01 +0530

    sd

--- src/ccavRequestHandler.js
+++ src/ccavRequestHandler.js
@@ -182,6 +182,7 @@ const create_PlanPayment = async (PlanId, body, ccavenue) => {
     let PlanPrice = parseInt(Plan.Price) - discound;
     let PaidAmount = Plan.PaidAmount ? Plan.PaidAmount : 0;
     let ToBePaid = PaidAmount + body.amount;
+    console.log(ToBePaid, body.amount)
     ToBePaid = ToBePaid == null ? 0 : ToBePaid
     console.log(PaidAmount,ToBePaid)
     let finding = await PlanPayment.find().count();
