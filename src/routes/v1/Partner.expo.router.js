1c9bc3f7024c3b62dc21c709bd3370b6ec0efc5a
muthamizhyadav <muthamizhyadav@gmail.com>
2023-10-03 17:04:23 +0530

    asdf

--- src/routes/v1/Partner.expo.router.js
+++ src/routes/v1/Partner.expo.router.js
@@ -8,5 +8,5 @@ router.route('/').post(PartnerController.createPartner);
 router.route('/admin/:page').get(PartnerController.gePartnersAll);
 router.route('/:id').put(PartnerController.updatePartnersById);
 router.route('/plane/creation').post(PartnerController.createPlanes);
-router.route('/gePartners/Planes/All/:page').get(PartnerController.gePartnersAll);
+router.route('/gePartners/Planes/All/:page').get(PartnerController.gePartnersPlanesAll);
 module.exports = router;
