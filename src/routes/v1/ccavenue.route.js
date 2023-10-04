39c6b902f3b7fbfd67abf1a007fb5b1f89a15bc3
bharathiuyarchi <bharathi@uyarchi.com>
2023-10-04 10:12:30 +0530

    dc

--- src/routes/v1/ccavenue.route.js
+++ src/routes/v1/ccavenue.route.js
@@ -8,7 +8,7 @@ router.route('/paynow/encript/value').get(ccavenue.pay_now_encript_value);
 router.route('/exhibitor/purchese/plan').get(ccavenue.exhibitor_purchese_plan);
 router.route('/nearby').get(ccavenue.nearby_value);
 
-router.route('/get/payment/response').get(ccavenue.get_paymant_success_response);
+router.route('/get/payment/response/:id').get(ccavenue.get_paymant_success_response);
 
 
 
