d8e516f15caf409a120ec586968a9fa4714e9fe2
bharathiuyarchi <bharathi@uyarchi.com>
2023-10-09 10:53:38 +0530

    d

--- src/services/ecomplan.service.js
+++ src/services/ecomplan.service.js
@@ -1566,7 +1566,7 @@ const create_stream_one = async (req) => {
   if (slot_booking.status == 'Booked') {
     throw new ApiError(httpStatus.NOT_FOUND, 'SLot Alredy Booked');
   }
-  slot_booking = await SlotBooking.findByIdAndUpdate({ _id: slot._id }, { status: 'Booked' }, { new: true });
+  slot_booking = await SlotBooking.findByIdAndUpdate({ _id: slot_booking._id }, { status: 'Booked' }, { new: true });
   let current_deta = new Date().getTime();
   let slot = await Slot.findById(slot_booking.slotId);
   if (slot.end > current_deta) {
